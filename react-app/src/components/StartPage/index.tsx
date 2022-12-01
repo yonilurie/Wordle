dcdfb414a95520e74ec2b29c175e0f0780c9f3b7
yonilurie <yonilurie@protonmail.com>
2022-12-01 14:35:54 -0800

    Added background styling for html and body

--- react-app/src/components/StartPage/index.tsx
+++ react-app/src/components/StartPage/index.tsx
@@ -19,6 +19,7 @@ const StartPage: FC<Props> = ({
 			setGameOn(true);
 		}, 300);
 	};
+	document.documentElement.style.backgroundColor = "rgb(227, 227, 225)";
 	return (
 		<div className={`start-page ${showStartPage ? "" : "hidden"}`}>
 			<div className="start-page-content">
